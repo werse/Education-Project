de3701ed64020c66613306d7df1e7fb9210285d2
frenky <frenkypyps@gmail.com>
2017-09-10 19:41:43 +0300

    rename

--- src/test/java/figure/RectangleTest.java
+++ src/test/java/figure/RectangleTest.java
@@ -9,14 +9,14 @@ public class RectangleTest {
 
     @Test
     public void getSquareTest() throws Exception {
-        Figure square = new Rectangle(10, 10);
-        assertThat(square.calculateSquare(), is(100.0));
+        Figure rectangle = new Rectangle(10, 10);
+        assertThat(rectangle.calculateSquare(), is(100.0));
     }
 
     @Test
     public void getSquareTestWithZeroParameters() throws Exception {
-        Figure square = new Rectangle(0, 0);
-        assertThat(square.calculateSquare(), is(0));
+        Figure rectangle = new Rectangle(0, 0);
+        assertThat(rectangle.calculateSquare(), is(0));
     }
 
     @Test(expected = IllegalArgumentException.class)
