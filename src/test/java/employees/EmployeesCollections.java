3dec73bed51759bca0465bd0c22a091d476d39ec
allizz <31736831+allizz@users.noreply.github.com>
2017-10-15 14:58:48 +0300

    i give up (#4)
    * не лопни
    * fixes

--- src/test/java/employees/EmployeesCollections.java
+++ src/test/java/employees/EmployeesCollections.java
@@ -1,15 +1,23 @@
 package employees;
 
+import com.google.common.collect.Lists;
 import org.junit.Test;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
+import java.util.TreeMap;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
+import static employees.EmployeesCollections.AgeCategory.MEDIUM;
+import static employees.EmployeesCollections.AgeCategory.OLD;
+import static employees.EmployeesCollections.AgeCategory.YOUNG;
 import static employees.Worker.workerBuilder;
 import static java.time.Instant.ofEpochMilli;
 import static java.time.LocalDate.parse;
@@ -19,7 +27,15 @@ import static java.util.Collections.emptyList;
 import static java.util.Collections.emptyMap;
 import static java.util.Collections.singletonList;
 import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toMap;
 import static java.util.stream.IntStream.range;
+import static org.hamcrest.Matchers.both;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.Matchers.lessThan;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
 
 public class EmployeesCollections {
 
@@ -49,22 +65,14 @@ public class EmployeesCollections {
     Boss moses = new Boss(mosesPollard, singletonList(jamesMaser), "Microsoft", 40);
 
     List<Employee> employees = asList(james, anna, john, moses);
-
-    String company = "Google";
-    List<Employee> employeesByCompany = getEmployeesByCompany(employees, company);
-    Map<String, List<Employee>> grouppedEmployees = groupEmployeesByCompany(employees);
   }
 
   @Test
   public void randomDataFiltering() throws Exception {
     List<Worker> googleWorkers = generateWorkers(1000);
-
     List<Worker> googleManagerWorkers = generateWorkers(10);
-
     Worker googleBossWorker = generateRandomWorker();
-
     AtomicInteger counter = new AtomicInteger(0);
-
     Boss googleBoss = new Boss(googleBossWorker, googleManagerWorkers, "Google", 50);
 
     List<Manager> googleManagers = googleManagerWorkers.stream()
@@ -84,9 +92,31 @@ public class EmployeesCollections {
       .flatMap(Collection::stream)
       .collect(toList());
 
-    System.out.println(employees);
-    groupByAgeCategory(googleWorkers);
-    groupByEmail(googleWorkers);
+    Map<AgeCategory, List<Worker>> stringListMap = groupByAgeCategory(googleWorkers);
+
+    stringListMap.get(YOUNG).forEach(worker ->
+      assertThat(getAge(worker), lessThan(35L)));
+
+    stringListMap.get(MEDIUM).forEach(worker ->
+      assertThat(getAge(worker), is(both(greaterThanOrEqualTo(35L)).and(lessThan(55L)))));
+
+    stringListMap.get(OLD).forEach(worker -> assertThat(getAge(worker), greaterThanOrEqualTo(55L)));
+
+    groupByWorkExperience(employees);
+    System.out.println();
+    groupByWorkExperienceFunctional(employees);
+
+    Map<String, List<Worker>> groupedByEmail1 = groupByEmail(googleWorkers);
+    assertNotNull(groupedByEmail1);
+    assertFalse(groupedByEmail1.isEmpty());
+
+    Map<String, List<Worker>> groupedByEmail2 = groupByEmailFunctional(googleWorkers);
+    assertNotNull(groupedByEmail2);
+    assertFalse(groupedByEmail2.isEmpty());
+  }
+
+  private static long getAge(Worker worker) {
+    return ChronoUnit.YEARS.between(worker.getDateOfBirth(), LocalDate.now());
   }
 
   private List<Employee> getEmployeesByCompany(List<Employee> employees, String company) {
@@ -99,21 +129,86 @@ public class EmployeesCollections {
     return emptyMap();
   }
 
-  private Map<String, List<Worker>> groupByAgeCategory(List<Worker> workers) {
-    //Young = "young" from 20 to 35 [20; 35)
-    //Old = "old" more that 55 [55; +inf)
-    //Medium = "medium" from 35 to 55 [35; 55)
-    return emptyMap();
+  private Map<AgeCategory, List<Worker>> groupByAgeCategory(List<Worker> workers) {
+    List<Worker> youngWorkers = new ArrayList<>();
+    List<Worker> middleAgedWorkers = new ArrayList<>();
+    List<Worker> oldWorkers = new ArrayList<>();
+
+    Map<AgeCategory, List<Worker>> group = new HashMap<>();
+
+    workers.forEach(worker -> {
+      long age = getAge(worker);
+
+      if (age >= 20 && age < 35) youngWorkers.add(worker);
+      if (age >= 35 && age < 55) middleAgedWorkers.add(worker);
+      if (age >= 55) oldWorkers.add(worker);
+    });
+
+    group.put(YOUNG, youngWorkers);
+    group.put(MEDIUM, middleAgedWorkers);
+    group.put(OLD, oldWorkers);
+
+    return group;
   }
 
   private Map<String, List<Worker>> groupByEmail(List<Worker> workers) {
-    return emptyMap();
+    Map<String, List<Worker>> group = new HashMap<>();
+
+    workers.forEach(worker -> {
+      String email = worker.getEmail();
+      if (group.containsKey(email)) {
+        group.get(email).add(worker);
+      } else {
+        group.put(email, new ArrayList<>(singletonList(worker)));
+      }
+    });
+
+    return group;
   }
 
-  private Map<Integer, List<Worker>> groupByWorkExperience(List<Worker> workers) {
-    // Work experience 1 year -> 1 worker
-    // Work experience 10 years -> 2 workers
-    return emptyMap();
+  private Map<String, List<Worker>> groupByEmailFunctional(List<Worker> workers) {
+    return workers.stream().collect(toMap(Worker::getEmail, Lists::newArrayList, this::addElementsToList));
+  }
+
+  private <T extends Worker> List<T> addElementsToList(List<T> initialList, List<T> addedValue) {
+    initialList.addAll(addedValue);
+    return initialList;
+  }
+
+  private Map<Integer, Integer> groupByWorkExperience(List<Employee> employees) {
+    Map<Integer, Integer> group = new TreeMap<>();
+    employees.forEach(worker -> {
+      int value = group.containsKey(worker.getExperience()) ? (group.get(worker.getExperience()) + 1) : 1;
+      group.put(worker.getExperience(), value);
+    });
+
+    printWorkers(group);
+    return group;
+  }
+
+  private Map<Integer, Integer> groupByWorkExperienceFunctional(List<Employee> workers) {
+    Map<Integer, Integer> group = workers.stream()
+      .collect(toMap(Employee::getExperience, e -> 1, this::addValue, TreeMap::new));
+    printWorkers(group);
+    return group;
+  }
+
+  private Integer addValue(Integer oldVal, Integer newVal) {
+    return oldVal + newVal;
+  }
+
+  private static void printWorkers(Map<Integer, Integer> group) {
+    System.out.println("work exp - employee");
+    group.forEach((key, value) -> {
+      if (key == 1 && value == 1)
+        System.out.println(key + " year - " + value + " worker");
+      else if (key == 1)
+        System.out.println(key + " year - " + value + " workers");
+      else if (value == 1)
+        System.out.println(key + " years - " + value + " worker");
+      else
+        System.out.println(key + " years - " + value + " workers");
+    });
   }
 
   // TODO: 25.09.2017 Organization as class with name, phone and location
@@ -144,5 +239,9 @@ public class EmployeesCollections {
     long max = 946_587_600_000L;
     long min = -946_782_000_000L;
     return min + ((long) (new Random().nextDouble() * (max + 1 - min)));
+  }
+
+  enum AgeCategory {
+    YOUNG, MEDIUM, OLD
   }
 }
